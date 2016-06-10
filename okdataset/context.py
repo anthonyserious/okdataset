4ed13f97b8954266464d2c282cdca47818750222
Anthony Schneider <anthony.schneider@electronifie.com>
2016-06-10 08:29:18 -0400

    Rename okdataset.yml to config.yml

--- okdataset/context.py
+++ okdataset/context.py
@@ -7,7 +7,7 @@ import yaml
 DataSet context
 """
 class Context(object):
-    def __init__(self, config=os.path.dirname(os.path.realpath(__file__)) + "/../okdataset.yml"):
+    def __init__(self, config=os.path.dirname(os.path.realpath(__file__)) + "/../config.yml"):
         self.config = yaml.load(open(config).read())
         
         self.bufferSize = config["cache"]["bufferSize"]
