5c67a43c9c4c87717e0f2e1959f52971d92df5b6
Anthony Schneider <anthonyserious@gmail.com>
2016-06-11 15:15:30 -0400

    Add map example.

---
+++ example/maparray.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+from okdataset.clist import ChainableList
+from okdataset.context import Context
+from okdataset.dataset import DataSet
+from okdataset.logger import Logger
+
+logger = Logger("maparray example")
+
+context = Context()
+logger.info("Building big list")
+l = ChainableList([ x for x in xrange(0, 1000000) ])
+
+logger.info("Building dataset")
+ds = DataSet(context, "big list", l)
+
+logger.info("Calling map")
+ds.map(lambda x: x * 2)
+
+logger.info("All done!")
+
