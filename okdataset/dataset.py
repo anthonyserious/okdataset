9ff9c40b26b010e7ea6ef316b75cdc696acb6bbb
Anthony Schneider <anthony.schneider@electronifie.com>
2016-06-13 15:11:05 -0400

    dataset-level profiling

--- okdataset/dataset.py
+++ okdataset/dataset.py
@@ -1,7 +1,9 @@
 from okdataset.clist import ChainableList
 from okdataset.logger import Logger
+from okdataset.profiler import Profiler, Timer
 
 from cloud.serialization.cloudpickle import dumps as pickle_dumps
+import json
 import pickle
 from itertools import groupby
 import zmq
@@ -14,6 +16,10 @@ class DataSet(ChainableList):
         self.context = context
         self.cache = context.cache
         self.label = label
+        
+        localTimer = Timer()
+
+        self.profiler = Profiler()
         
         if clist is None and not fromExisting:
             raise ValueError("Must provide either clist or fromExisting")
@@ -46,6 +52,7 @@ class DataSet(ChainableList):
             # Python 3
             raw_input = input
         
+        zmqTimer = Timer()
         context = zmq.Context()
 
         self.sender = context.socket(zmq.PUSH)
@@ -55,6 +62,8 @@ class DataSet(ChainableList):
         self.sink = context.socket(zmq.PULL)
         self.sink.bind("tcp://*:" + str(self.context.config["cluster"]["return"]["port"]))
         self.logger.debug("Initialized sink socket")
+        
+        self.profiler.add("localZmq", zmqTimer.since())
 
         """
         Store the current working dataset label.  This will change as new intermediary
@@ -71,20 +80,35 @@ class DataSet(ChainableList):
             for i in xrange(0, self.buffers + 1):
                 start = self.bufferSize * i
                 end = self.bufferSize * (i + 1)
-
+                
+                cacheTimer = Timer()
                 self.cache.pushBuffer(label, i, pickle_dumps(ChainableList(clist[start:end])))
+                self.profiler.add("masterCache", cacheTimer.since())
             
             self.logger.debug("Initialized with %d" % self.buffers)
+            self.logger.debug(json.dumps(self.profiler.toDict(), indent=2))
 
     def createIntermediary(self):
         prefix = self.label + "_intermediary_"
+        
+        timer = Timer()
+        
         self.currentDsLabel = prefix + str(self.cache.incr(prefix))
+        
+        self.profiler.add("masterCache", timer.since())
+        
         return self.currentDsLabel
 
     def map(self, f):
         self.logger.debug("Starting map on %s" % self.currentDsLabel)
 
+        self.profiler = Profiler()
+        localTimer = Timer()
+        
+        cacheTimer = Timer()
         keys = self.cache.getKeys(self.currentDsLabel)
+        self.profiler.add("masterCache", cacheTimer.since())
+
         self.logger.debug("Got %d keys" % len(keys))
         
         source = self.currentDsLabel
@@ -92,22 +116,34 @@ class DataSet(ChainableList):
         
         for key in keys:
             self.logger.trace("Sending key %s" % key)
+            
+            zmqTimer = Timer()
             self.sender.send(pickle_dumps({
                 "method": "map",
                 "fn": f,
                 "offset": key,
                 "sourceLabel": source,
                 "destLabel": dest
-        }))
+            }))
+            self.profiler.add("masterZmq", zmqTimer.since())
 
         results = 0
 
         while results != len(keys) - 1:
             self.logger.trace("Received %d out of %d results" % (results, len(keys) - 1))
+
+            zmqTimer = Timer()
+            
             res = self.sink.recv_pyobj()
+            
+            self.profiler.add("masterZmq", zmqTimer.since())
+            
             results = results + 1
         
-        self.logger.debug("map complete")
+        self.profiler.add("masterOverall", localTimer.since())
+        
+        self.logger.info("map complete")
+        self.logger.debug(json.dumps(self.profiler.toDict(), indent=2))
 
     def filter(self, f):
         return ChainableList(filter(f, self[:]))
@@ -129,4 +165,11 @@ class DataSet(ChainableList):
 
     def collect(self):
         pass
+
+
+    def getProfile(self, f=None):
+        if f:
+            f(self.profiler.toDict())
+        else:
+            return self.profiler.toDict()
 
---
+++ okdataset/profiler.py
@@ -0,0 +1,49 @@
+import time
+
+class Time(object):
+    def __init__(self, t=None, c=None):
+        if t is not None and c is not None:
+            self.time = t
+            self.cpu = c
+        else:
+            self.time = time.time()
+            self.cpu = time.clock()
+
+    def __add__(self, t):
+        return Time(
+            self.time + t.time, 
+            self.cpu + t.cpu
+        )
+
+    def toDict(self):
+        return { "time": self.time, "cpu": self.cpu }
+
+class Timer(object):
+    def __init__(self):
+        self.t = Time()
+
+    def since(self):
+        return Time(
+            t = time.time() - self.t.time,
+            c = time.clock() - self.t.cpu
+        )
+
+    def reset(self):
+        self.t = Time()
+
+class Profiler(object):
+    def __init__(self):
+        self.timings = {}
+
+    def add(self, key, t):
+        if key not in self.timings:
+            self.timings[key] = t
+        else:
+            self.timings[key] += t
+    
+    def getTimings(self):
+        return self.timings
+
+    def toDict(self):
+        return dict((k, v.toDict()) for k, v in self.timings.iteritems())
+
