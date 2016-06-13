3752460d4aafdbece6b4928bbaf2043fb2c5dd0c
Anthony Schneider <anthony.schneider@electronifie.com>
2016-06-13 10:55:47 -0400

    Limit log messages by LOG_LEVEL environment variable.

--- okdataset/logger.py
+++ okdataset/logger.py
@@ -33,6 +33,9 @@ class Logger(object):
         
         self.hostname = platform.node()
         self.pid = os.getpid()
+
+        # default to INFO level logging
+        self.level = int(os.environ.get("LOG_LEVEL", 30))
         
     def _pre(self, fields, msg=None):
         log = {
@@ -56,31 +59,43 @@ class Logger(object):
         sys.stdout.flush()
         
     def fatal(self, fields, msg=None):
+        if self.level > 60:
+            return
         log = self._pre()
         log["level"] = 60
         self._print(log)
         
     def error(self, fields, msg=None):
+        if self.level > 50:
+            return
         log = self._pre(fields, msg=msg)
         log["level"] = 50
         self._print(log)
         
     def warn(self, fields, msg=None):
+        if self.level > 40:
+            return
         log = self._pre(fields, msg=msg)
         log["level"] = 40
         self._print(log)
         
     def info(self, fields, msg=None):
+        if self.level > 30:
+            return
         log = self._pre(fields, msg=msg)
         log["level"] = 30
         self._print(log)
 
     def debug(self, fields, msg=None):
+        if self.level > 20:
+            return
         log = self._pre(fields, msg=msg)
         log["level"] = 20
         self._print(log)
 
     def trace(self, fields, msg=None):
+        if self.level > 10:
+            return
         log = self._pre(fields, msg=msg)
         log["level"] = 10
         self._print(log)
