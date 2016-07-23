e4560d7ac04ae26cf8a87be6c9ddbace061a71e9
Anthony Schneider <anthony.schneider@electronifie.com>
2016-07-23 12:11:37 -0400

    Examples modified for new DataSet calling convention

--- example/mapFromExisting.py
+++ example/mapFromExisting.py
@@ -2,14 +2,13 @@
 
 from okdataset.clist import ChainableList
 from okdataset.context import Context
-from okdataset.dataset import DataSet
 from okdataset.logger import Logger
 
 logger = Logger("map from existing example")
 
 context = Context()
 logger.info("Building dataset from existing")
-ds = DataSet(context, "big list", fromExisting=True)
+ds = context.dataSet(label="big list", fromExisting=True)
 
 logger.info("Calling map")
 ds.map(lambda x: x * 2)
--- example/maparray.py
+++ example/maparray.py
@@ -2,7 +2,6 @@
 
 from okdataset.clist import ChainableList
 from okdataset.context import Context
-from okdataset.dataset import DataSet
 from okdataset.logger import Logger
 
 logger = Logger("maparray example")
@@ -12,7 +11,7 @@ logger.info("Building big list")
 l = ChainableList([ x for x in xrange(0, 1000000) ])
 
 logger.info("Building dataset")
-ds = DataSet(context, "big list", l)
+ds = context.dataSet(l, label="big list")
 
 logger.info("Calling map")
 ds.map(lambda x: x * 2)
