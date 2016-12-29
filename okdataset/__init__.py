4e6f39006a5bd9b8ac4e136545b5214191140b91
Anthony Schneider <anthony.schneider@electronifie.com>
2016-12-29 11:12:34 -0500

    Fixing compose issues and examples

--- okdataset/__init__.py
+++ okdataset/__init__.py
@@ -1,1 +1,6 @@
+from okdataset.clist import ChainableList
+from okdataset.context import Context
+from okdataset.dataset import DataSet
+from okdataset.logger import Logger
 
+__all__ = [ ChainableList, Context, DataSet, Logger ]
